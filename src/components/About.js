0572f01deda7b0661258f56d34a2b784d8edfb66
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-05-09 10:04:22 +0300

    completed assignment

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function About({src,aboutText}){
+    return(
+        <aside>
+            <img src="https://via.placeholder.com/215" alt="blog logo"/>
+            <p>{aboutText}</p>
+        </aside>
+
+    )
+}
+
+export default About ;
